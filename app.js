c48e115ab2ebf1ecf9be1bf14e95a8cbc1002a37
Pawluch <dawid.wijata@hotmail.com>
2021-02-10 10:27:10 +0100

    Make the routes from tasks work properly

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 import { route } from './router';
 
-route('/', 'home', function() {
-  this.where = 'here';
-});
-
 route('/ex1', 'example1', function() {
   this.title = 'Example 1';
 });
@@ -15,6 +11,61 @@ route('/ex2', 'example2', function() {
     this.counter += 1;
     this.$refresh();
   });
+});
+
+route('/success', 'success', function() {
+  this.title = 'Successful login!';
+  this.logout = 'Log out';
+
+  this.$on('.logout-btn', 'click', () => {
+    window.location = '#';
+  });
+});
+
+route('/', 'login', function () {
+  this.title = "Login form";
+  this.errorMsg = null;
+  this.responseToken = null;
+
+  this.$on('.login-form', 'submit', async (evt) => {
+    const formData = new FormData(document.querySelector('.login-form'));
+    const formDataObject = Object.fromEntries(formData.entries());
+    const loginApiURL = 'https://zwzt-zadanie.netlify.app/api/login';
+
+    evt.preventDefault();
+
+    // double security is still better than none :)
+    if(formDataObject.username.length === 0 && formDataObject.password.length === 0){
+      this.errorMsg = 'Enter password and login!';
+      this.$refresh();
+      return;
+    }
+
+    if(formDataObject.username.length === 0){
+      this.errorMsg = 'Enter your login!';
+      this.$refresh();
+      return;
+    }
+
+    if(formDataObject.password.length === 0){
+      this.errorMsg = 'Enter your password!';
+      this.$refresh();
+      return;
+    }
+
+    const response = await fetch(loginApiURL, {method: 'POST', body: JSON.stringify(formDataObject)});
+
+    if(response.status >= 200 && response.status <= 299){
+      window.location = '#/success';
+    }
+    else{
+      const responseJSON = await response.json();
+
+      this.errorMsg = responseJSON.message;
+      this.$refresh();
+    }
+  });
+
 });
 
 route('*', '404', function () {});
